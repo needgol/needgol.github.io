2728f9cfafad6328188e019dd8760aebc7393e0b
Giovany <148998589+needgol@users.noreply.github.com>
2024-10-11 10:51:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-$.ajax({
-  url: 'https://v2.sparktraffic.com/balance',
-  crossDomain: true,
+fetch('https://v2.sparktraffic.com/balance', {
   headers: {
     'accept': 'application/json',
     'api_key': 'f3eff12541ace4adf69b47b58b03a6a1'
   }
-}).done(function(response) {
-  console.log(response);
 });
