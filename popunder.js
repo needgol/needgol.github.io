fb4e237f7f17ce48a90402dd1d3786891d4de46b
Giovany <148998589+needgol@users.noreply.github.com>
2024-10-17 12:48:14 -0300

    Update and rename script.js to popunder.js

---
+++ popunder.js
@@ -0,0 +1,15 @@
+// pop under window script generated at http://snapbuilder.com/
+
+function goWin() {
+var mh = 624;
+var mw = 824;
+TheWin = window.open('','image','height=' + mh + ',width=' + mw + ',toolbar=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no');
+TheWin.resizeTo(mw+2,mh+30);
+TheWin.document.write('<!doctype html public "-//w3c//dtd xhtml 1.0Transitional//en" "http://www.w3.org/tr/xhtml1/dtd/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml">');
+TheWin.document.write('<head><title>Pop Under</title></head><body style="overflow:hidden" bgcolor="#000000">');
+TheWin.document.write('https:\/\/needgol.github.io\/
+https:\/\/needgol.github.io\/page
+https:\/\/needgol.github.io\/page-exemplo.html<div align="center"><a href="#" onclick="self.close();return false;">Close Window</a></div></body></html>');
+TheWin.moveTo(100,100);
+TheWin.focus();
+}
--- script.js
+++
@@ -1,6 +0,0 @@
-var popunder="http://needgol.github.io/page-exemplo.html/"
-function loadpopunder(){
-win2=window.open(popunder[Math.floor(Math.random()*(popunder.length))])
-win2.blur()
-window.focus()
-}
