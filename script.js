b4c2111463d0032648634f4d266a8e6b765acdef
Giovany <148998589+needgol@users.noreply.github.com>
2024-10-17 12:34:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var popunder="http://needgol.github.io/page-exemplo.html/"
 function loadpopunder(){
-win2=window.open(popunder)
+win2=window.open(popunder[Math.floor(Math.random()*(popunder.length))])
 win2.blur()
 window.focus()
 }
