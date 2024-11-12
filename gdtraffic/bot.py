b0bf3a1f10e36c44a9fda6e3fefaf4fb3883be97
Giovany <148998589+needgol@users.noreply.github.com>
2024-11-12 11:46:07 -0300

    Update bot.py

--- gdtraffic/bot.py
+++ gdtraffic/bot.py
@@ -4,8 +4,8 @@ import webbrowser
 import time
 x = 1
 while True:
-  webbrowser.open('http://url/')
-  time.sleep(15)
+  webbrowser.open('https://needgol.github.io/mastersurf.html')
+  time.sleep(10)
 	webbrowser.open_new_tab('https://needgol.github.io/')
 	time.sleep(15)
 	webbrowser.open_new_tab('https://needgol.github.io/page/')
